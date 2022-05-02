6f318f4d92cefbd2e640ed60bc20b52303be0c2f
Kengo Seki <sekikn@apache.org>
2022-05-02 12:35:48 +0900

    fix

--- integration/airflow/tests/integration/docker/up-2.sh
+++ integration/airflow/tests/integration/docker/up-2.sh
@@ -34,6 +34,7 @@ cat > requirements.txt <<EOL
 airflow-provider-great-expectations==0.0.8
 apache-airflow-providers-snowflake==2.5.1
 apache-airflow-providers-postgres==3.0.0
+apache-airflow-providers-jdbc>=2.0.0
 apache-airflow-providers-google==6.7.0
 great-expectations==0.13.42
 dbt-core==1.0.1
